0d0fd0033ff937b43cb0fadef75154401bf6ad9c
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-08 00:34:49 +0600

    17 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"strconv"
 
 	"log"
 
-	"github.com/AltynayK/firstpraktikum/internal/handler"
+	"github.com/AltynayK/firstpraktikum/internal/app"
 	"github.com/gorilla/mux"
 )
 
@@ -13,6 +15,59 @@ var (
 	IDList map[int]string
 	id     int
 )
+
+func Post(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("content-type", "plain/text")
+
+	b, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	url := app.ShortURL(string(b))
+	if IDList == nil {
+		IDList = make(map[int]string)
+	}
+
+	id++
+	IDList[id] = url
+
+	//log.Print(id)
+
+	//log.Print(IDList)
+	w.WriteHeader(201)
+
+	w.Write([]byte(url))
+
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		w.WriteHeader(400)
+	}
+	//fmt.Println(`id := `, id)
+
+	b, err := strconv.Atoi(id)
+	if err != nil && b < 1 {
+		w.WriteHeader(400)
+		return
+	}
+	w.WriteHeader(307)
+	w.Header().Set("Location", app.LongURL(IDList[b]))
+
+	//w.Header.WriteSubset(w io.Writer, app.LongUrl(IdList[id]))
+
+	log.Print(app.LongURL(IDList[b]))
+
+	//w.Write([]byte(app.LongURL(IDList[b])))
+	//w.Write([]byte(app.LongURL(IDList[id])))
+}
 
 const port = ":8080"
 
@@ -34,8 +89,8 @@ func initHandlers() *mux.Router {
 	// TODO: handle "Not Allowed Method" example: DELETE method request to /
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", handler.Post).Methods("POST")
-	router.HandleFunc("/{id}", handler.Get).Methods("GET")
+	router.HandleFunc("/", Post).Methods("POST")
+	router.HandleFunc("/{id}", Get).Methods("GET")
 
 	return router
 }
