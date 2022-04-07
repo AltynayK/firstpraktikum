291cc6b0de731a899876f2736fb3ba88384a72f6
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-07 13:02:48 +0600

    8 commit

--- internal/app/service.go
+++ internal/app/service.go
@@ -6,17 +6,17 @@ import (
 )
 
 func ShortURL(url string) string {
-	ShortUrl := base64.StdEncoding.EncodeToString([]byte(url))
+	ShortURL := base64.StdEncoding.EncodeToString([]byte(url))
 	//fmt.Println(ShortUrl)
-	return ShortUrl
+	return ShortURL
 }
 
 func LongURL(url string) string {
-	LongUrl, err := base64.StdEncoding.DecodeString(url)
+	LongURL, err := base64.StdEncoding.DecodeString(url)
 	if err != nil {
 		fmt.Println(err)
 	}
 	//fmt.Println(string(LongUrl))
-	return string(LongUrl)
+	return string(LongURL)
 
 }
