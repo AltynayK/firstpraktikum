c6e5d84f0018b9c72705a2bfcd7f5645d6ea59c1
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-07 22:48:37 +0600

    15 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -60,7 +60,7 @@ func Get(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Location", "http://"+app.LongURL(IDList[b]))
+	w.Header().Set("Location", "http://localhost:8080/"+app.LongURL(IDList[b]))
 
 	//
 
