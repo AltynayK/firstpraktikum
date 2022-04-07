031c6d7300142ad78af30a1077a4987f526ccb4d
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-08 00:54:29 +0600

    18 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -49,13 +49,15 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, ok := vars["id"]
 	if !ok {
-		//	w.WriteHeader(400)
+
+		w.WriteHeader(400)
+		return
 	}
 	//fmt.Println(`id := `, id)
 
 	b, err := strconv.Atoi(id)
 	if err != nil && b < 1 {
-		//w.WriteHeader(400)
+		w.WriteHeader(400)
 		return
 	}
 	//w.WriteHeader(307)
