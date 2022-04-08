6296025e0751e153e6a6aecb0503046f2605966f
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-09 01:17:07 +0600

    21 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 
 	"github.com/AltynayK/firstpraktikum/internal/app"
+	"github.com/gorilla/mux"
 )
 
 var (
@@ -26,46 +27,44 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	url := app.ShortURL(string(b))
+	longUrl := string(b)
 	if IDList == nil {
 		IDList = make(map[int]string)
 	}
 
 	id++
-	IDList[id] = url
-
+	IDList[id] = longUrl
+	shortUrl := app.ShortURL(strconv.Itoa(id))
 	//log.Print(id)
 
 	//log.Print(IDList)
 	w.WriteHeader(201)
 
-	w.Write([]byte(url))
+	w.Write([]byte(shortUrl))
 
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
 
-	id := r.URL.Query().Get("id")
-	if id == "" {
-		http.Error(w, "The query parameter is missing", http.StatusBadRequest)
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+
+		w.WriteHeader(400)
 		return
 	}
+	//fmt.Println(`id := `, id)
+
 	b, err := strconv.Atoi(id)
 	if err != nil && b < 1 {
 		w.WriteHeader(400)
 		return
-		//
 	}
-	//fmt.Fprintf(w, "Welcome to the home page!")
-	w.Header().Set("Location", app.LongURL(IDList[b]))
-
-	// Первый путь
-	// id := query["id"][0]
-
-	// Второй способ
+	longUrl := IDList[b]
+	//
+	w.Header().Set("Location", longUrl)
 
 	w.WriteHeader(307)
-
 	//w.Header.WriteSubset(w io.Writer, app.LongUrl(IdList[id]))
 
 	//log.Print(app.LongURL(IDList[b]))
@@ -74,37 +73,31 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	//w.Write([]byte(app.LongURL(IDList[id])))
 }
 
-// const port = ":8080"
+const port = ":8080"
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", Post)
-	mux.HandleFunc("/{id}", Get)
-	log.Fatal(http.ListenAndServe(":8080", mux))
-	// mux := initHandlers()
+	mux := initHandlers()
 	//IDList = make(map[int]string)
 
-	// srv := http.Server{
-	// 	Addr:    port,
-	// 	Handler: mux,
-	// }
+	srv := http.Server{
+		Addr:    port,
+		Handler: mux,
+	}
 
 	//log.Printf("App listening port: %s", port)
-	// log.Fatal(srv.ListenAndServe())
+	log.Fatal(srv.ListenAndServe())
 
 }
+func initHandlers() *mux.Router {
+	// TODO: how handler 404 (if not found some url, example: /not_exist_url)
+	// TODO: handle "Not Allowed Method" example: DELETE method request to /
 
-// func initHandlers() *mux.Router {
-// TODO: how handler 404 (if not found some url, example: /not_exist_url)
-// TODO: handle "Not Allowed Method" example: DELETE method request to /
+	router := mux.NewRouter()
+	router.HandleFunc("/", Post).Methods("POST")
+	router.HandleFunc("/{id}", Get).Methods("GET")
 
-// 	router := mux.NewRouter()
-// 	router.HandleFunc("/", Post).Methods("POST")
-// 	router.HandleFunc("/id", Get).Methods("GET")
-
-// 	return router
-// }
+	return router
+}
 
 // func WriteShortURLByID(url string) int {
 
