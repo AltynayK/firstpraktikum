39e7d9420cf573f46b5861b1cc40cf3419a58521
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-18 15:15:32 +0600

    commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -16,7 +16,7 @@ func main() {
 	mux := initHandlers()
 	//IDList = make(map[int]string)
 	os.Setenv("SERVER_ADDRESS", "127.0.0.1"+port)
-	os.Setenv("BASE_URL", "http://127.0.0.1:8080/")
+	os.Setenv("BASE_URL", "http://127.0.0.1"+port+"/")
 	srv := http.Server{
 		Addr:    port,
 		Handler: mux,
