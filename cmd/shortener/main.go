801b0bbffc3a4886a1b8dafc0a269bb6123ac893
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-25 12:42:21 +0600

    конфигурирование сервиса с помощью флагов командной строки наравне с уже имеющимися переменными окружения

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -27,13 +27,16 @@ func main() {
 	//IDList = make(map[int]string)
 
 	ServerAddress := flag.String("a", "127.0.0.1:8080", "SERVER_ADDRESS - адрес запуска HTTP-сервера")
+	BaseUrl := flag.String("b", "http://"+*ServerAddress, "BASE_URL")
 	flag.Parse()
-	if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
-		*ServerAddress = u
-	}
-
+	// if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
+	// 	*ServerAddress = u
+	// }
+	// if u, f := os.LookupEnv("BASE_URL"); f {
+	// 	*BaseUrl = u
+	// }
 	//os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
-	os.Setenv("BASE_URL", "http://"+*ServerAddress)
+	os.Setenv("BASE_URL", *BaseUrl)
 	srv := http.Server{
 		Addr:    *ServerAddress,
 		Handler: mux,
