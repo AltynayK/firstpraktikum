c76ffb2b2185540dc9f841dcbc04067236a5013f
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-26 18:34:47 +0600

    commit

--- internal/handler/zip_middleware.go
+++ internal/handler/zip_middleware.go
@@ -30,7 +30,7 @@ func CompressGzip(next http.Handler) http.Handler {
 		}
 		defer gz.Close()
 		w.Header().Set("Content-Encoding", "gzip")
-		w.Header().Set("Vary", "Accept-Encoding")
+		//w.Header().Set("Vary", "Accept-Encoding")
 		w.Header().Del("Content-Length")
 		next.ServeHTTP(gzipBodyWriter{
 			ResponseWriter: w,
