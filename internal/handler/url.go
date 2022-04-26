62e441bfb505dcc046a0be8d8666e08766e45d36
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-04-26 12:21:45 +0600

    поддержка  gzip в сервисе

--- internal/handler/url.go
+++ internal/handler/url.go
@@ -1,16 +1,50 @@
 package handler
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/AltynayK/firstpraktikum/internal/service"
 	"github.com/AltynayK/firstpraktikum/internal/short"
 	"github.com/gorilla/mux"
 )
+
+type gzipBodyWriter struct {
+	http.ResponseWriter
+	writer io.Writer
+}
+
+func (gz gzipBodyWriter) Write(b []byte) (int, error) {
+	return gz.writer.Write(b)
+}
+
+func CompressGzip(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestCompression)
+		if err != nil {
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+		defer gz.Close()
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Vary", "Accept-Encoding")
+		w.Header().Del("Content-Length")
+		next.ServeHTTP(gzipBodyWriter{
+			ResponseWriter: w,
+			writer:         gz,
+		}, r)
+	})
+}
 
 type URL struct {
 	LongURL string `json:"url"`
