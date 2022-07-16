eb733b293283b58297f39bf4b13a767fca1bd689
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-07-16 12:45:02 +0600

    убрала создание таблицы в мидлвере

--- internal/postgresql/postgresql.go
+++ internal/postgresql/postgresql.go
@@ -20,10 +20,10 @@ func InsertDataToDB(short_url string, original_url string, user_id string) {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	// _, err = db.Exec("CREATE TABLE IF NOT EXISTS data (id serial primary key, short_url varchar, original_url varchar, user_id varchar)")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS data (id serial primary key, short_url varchar, original_url varchar, user_id varchar)")
+	if err != nil {
+		panic(err)
+	}
 	sqlStatement := `INSERT INTO data (short_url, original_url, user_id) VALUES ($1, $2, $3)`
 	_, err = db.Exec(sqlStatement, short_url, original_url, user_id)
 	if err != nil {
