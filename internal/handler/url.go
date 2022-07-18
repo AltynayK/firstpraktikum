66f260e930f7d438ec9cc9b9ed7594f20d7d7f45
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-07-18 17:37:37 +0600

    поменяла местами условие по переменной окружения

--- internal/handler/url.go
+++ internal/handler/url.go
@@ -60,8 +60,8 @@ func PostJSON(w http.ResponseWriter, r *http.Request) {
 
 	a := r.Context().Value(userCtxKey).(string)
 
-	_, exists := os.LookupEnv("DatabaseDNS")
-	if !exists {
+	//_, exists := os.LookupEnv("DatabaseDNS")
+	if DBdns == nil {
 		ShortURL = short.WriteShortURL(url.LongURL)
 		service.MakeData(url.LongURL, ShortURL, a)
 		w.WriteHeader(201)
@@ -100,8 +100,8 @@ func PostText(w http.ResponseWriter, r *http.Request) {
 
 	a := r.Context().Value(userCtxKey).(string)
 
-	_, exists := os.LookupEnv("DatabaseDNS")
-	if !exists {
+	//_, exists := os.LookupEnv("DatabaseDNS")
+	if DBdns == nil {
 		shortURL = short.WriteShortURL(longURL)
 		service.MakeData(longURL, shortURL, a)
 		w.WriteHeader(201)
