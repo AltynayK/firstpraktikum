cd234799211bad075197cb1eaf8fc762941f5095
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-08-05 18:21:14 +0600

    add ping check for post json and text

--- internal/handler/url.go
+++ internal/handler/url.go
@@ -61,23 +61,23 @@ func PostJSON(w http.ResponseWriter, r *http.Request) {
 	a := r.Context().Value(userCtxKey).(string)
 
 	//_, exists := os.LookupEnv("DatabaseDNS")
-	// if repository.Ping() == true {
-	// 	ShortURL = short.WriteShortURL(url.LongURL)
-	// 	//service.WriteURLByID(url.LongURL)
+	if repository.Ping() == true {
+		ShortURL = short.WriteShortURL(url.LongURL)
+		//service.WriteURLByID(url.LongURL)
 
-	// 	if repository.InsertDataToDB(ShortURL, url.LongURL, a) == false {
-	// 		ShortURL = repository.ReturnShortURL(url.LongURL)
-	// 		w.WriteHeader(409)
-	// 	} else {
-	// 		//service.MakeData(url.LongURL, ShortURL, a)
-	// 		w.WriteHeader(201)
-	// 	}
+		if repository.InsertDataToDB(ShortURL, url.LongURL, a) == false {
+			ShortURL = repository.ReturnShortURL(url.LongURL)
+			w.WriteHeader(409)
+		} else {
+			//service.MakeData(url.LongURL, ShortURL, a)
+			w.WriteHeader(201)
+		}
 
-	// } else {
-	ShortURL = short.WriteShortURL(url.LongURL)
+	} else {
+		ShortURL = short.WriteShortURL(url.LongURL)
 
-	w.WriteHeader(201)
-	//}
+		w.WriteHeader(201)
+	}
 	service.MakeData(url.LongURL, ShortURL, a)
 	okRes := URL{
 		Result: ShortURL,
@@ -104,23 +104,23 @@ func PostText(w http.ResponseWriter, r *http.Request) {
 	a := r.Context().Value(userCtxKey).(string)
 
 	//_, exists := os.LookupEnv("DatabaseDNS") *DBdns != "" &&
-	// if repository.Ping() == true {
-	// 	shortURL = short.WriteShortURL(longURL)
-	// 	//service.WriteURLByID(longURL)
-	// 	if repository.InsertDataToDB(shortURL, longURL, a) == false {
-	// 		shortURL = repository.ReturnShortURL(longURL)
-	// 		w.WriteHeader(409)
-	// 	} else {
-	// 		//service.MakeData(longURL, shortURL, a)
-	// 		w.WriteHeader(201)
-	// 	}
+	if repository.Ping() == true {
+		shortURL = short.WriteShortURL(longURL)
+		//service.WriteURLByID(longURL)
+		if repository.InsertDataToDB(shortURL, longURL, a) == false {
+			shortURL = repository.ReturnShortURL(longURL)
+			w.WriteHeader(409)
+		} else {
+			//service.MakeData(longURL, shortURL, a)
+			w.WriteHeader(201)
+		}
 
-	// } else {
-	shortURL = short.WriteShortURL(longURL)
+	} else {
+		shortURL = short.WriteShortURL(longURL)
 
-	w.WriteHeader(201)
+		w.WriteHeader(201)
 
-	//}
+	}
 	service.MakeData(longURL, shortURL, a)
 	w.Header().Set("Location", shortURL)
 	w.Write([]byte(shortURL))
