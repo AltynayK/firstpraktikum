9105cebe1c050d2f54b235e513ebfb026fbc1ea1
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-09-22 21:54:18 +0600

    вывод конфига в отдельный файл

--- internal/app/config.go
+++ internal/app/config.go
@@ -9,16 +9,20 @@ type Config struct {
 	DatabaseDNS     string
 }
 
+var config *Config
+
 func NewConfig() *Config {
-	ServerAddress := flag.String("a", "127.0.0.1:8080", "ServerAddress - адрес запуска HTTP-сервера")
-	BaseURL := flag.String("b", "http://"+*ServerAddress, "BaseURL")
-	FileStoragePath := flag.String("f", "texts.txt", "FileStoragePath - путь до файла LongURL")
-	DatabaseDNS := flag.String("d", "", "DatabaseDNS")
+	flag.StringVar(&config.ServerAddress, "a", "127.0.0.1:8080", "ServerAddress - адрес запуска HTTP-сервера")
+	flag.StringVar(&config.BaseURL, "b", "http://"+config.ServerAddress, "BaseURL")
+	//increment#
+	flag.StringVar(&config.FileStoragePath, "f", "texts.txt", "FileStoragePath - путь до файла LongURL")
+	//flag.StringVar(&DatabaseDNS, "d", "host=localhost port=5432 user=altynay password=password dbname=somedb sslmode=disable", "DatabaseDNS")
+	flag.StringVar(&config.DatabaseDNS, "d", "", "DatabaseDNS")
 	flag.Parse()
 	return &Config{
-		ServerAddress:   *ServerAddress,
-		BaseURL:         *BaseURL,
-		FileStoragePath: *FileStoragePath,
-		DatabaseDNS:     *DatabaseDNS,
+		ServerAddress:   config.ServerAddress,
+		BaseURL:         config.BaseURL,
+		FileStoragePath: config.FileStoragePath,
+		DatabaseDNS:     config.DatabaseDNS,
 	}
 }
