c25e1bebebc3b727d7bc1222f569641d5128e435
AltynayK <altynaykurmangaliyeva@gmail.com>
2022-10-09 10:45:42 +0600

    изменены contect остановка гоурутин

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/AltynayK/firstpraktikum/internal/app"
@@ -16,15 +17,37 @@ func main() {
 	config := app.NewConfig()
 	s := handler.NewHandler(config)
 	s.Run(config)
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
+	parent, pCancel := context.WithCancel(context.Background())
+	child, _ := context.WithCancel(parent)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		go work(wg, child)
+	}
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+	defer signal.Stop(c)
 	select {
-	case <-time.After(10 * time.Second):
-		fmt.Println("missed signal")
-	case <-ctx.Done():
-		stop()
-		fmt.Println("signal received")
+	case <-c:
+		pCancel()
+		fmt.Println("Waiting everyone to finish...")
+		wg.Wait()
+		fmt.Println("Exiting")
+		os.Exit(0)
 	}
 
+}
+func work(wg *sync.WaitGroup, ctx context.Context) {
+	done := false
+	wg.Add(1)
+	for !done {
+		fmt.Println("Doing something...")
+		time.Sleep(time.Second)
+		select {
+		case <-ctx.Done():
+			fmt.Println("Done")
+			done = true
+		default:
+		}
+	}
+	wg.Done()
 }
